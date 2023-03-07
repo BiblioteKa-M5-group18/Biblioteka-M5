7d48bce88d9633105c96fec4b97ae2e475c1f6ea
M7uricio <m7uricio.dev@gmail.com>
2023-03-07 13:02:20 -0300

    feat: add rotes, serializer and views books

--- books/models.py
+++ books/models.py
@@ -6,11 +6,7 @@ from users.models import User
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=150)
-    pages = models.IntegerField(
-        validators=[
-        MaxLengthValidator(5)
-        ]
-    )
+    pages = models.CharField(max_length=5)
     publishing_company = models.CharField(max_length=50)
     user = models.ManyToManyField(
         User,
---
+++ books/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Book
+
+class BookSerializer(serializers.ModelSerializer):
+     def create(self, validated_data):
+        return Book.objects.create(**validated_data)
+     
+     class Meta:
+         model = Book
+         fields = ["id","title", "author", "pages", "publishing_company", "user"]
+         read_only_fields = ["user"]
