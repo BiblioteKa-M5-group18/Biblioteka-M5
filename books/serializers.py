a5a2d4b9edd12905193f651b2f502e8c0c973858
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-03-10 13:13:23 -0300

    fix: retornando id do book nas copias

--- books/serializers.py
+++ books/serializers.py
@@ -7,3 +7,9 @@ class BookSerializer(serializers.ModelSerializer):
         model = Book
         fields = ["id", "title", "author", "pages", "publishing_company"]
         read_only_fields = ["id"]
+
+
+class BookToCopySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ["id"]
