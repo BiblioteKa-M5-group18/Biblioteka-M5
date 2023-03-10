8952bebad0a0980d138049ddb1559addbbd8f56c
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-03-10 13:17:48 -0300

    fix: url da documentação

--- biblioteka/urls.py
+++ biblioteka/urls.py
@@ -34,5 +34,5 @@ urlpatterns = [
     path("api/", include("loans.urls")),
     path("api/", include("followings.urls")),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('api/docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
