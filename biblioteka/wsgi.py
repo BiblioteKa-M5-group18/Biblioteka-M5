98fd35cd63c5d45c46d0826dba6ff86c236f8cfd
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-03-10 15:29:37 -0300

    refactor: wsgi.oy

--- biblioteka/wsgi.py
+++ biblioteka/wsgi.py
@@ -1,4 +1,8 @@
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biblioteka.settings")
+
+import django
+django.setup()
 
 from django.core.wsgi import get_wsgi_application
 
@@ -6,7 +10,6 @@ from django_apscheduler.jobstores import register_events
 
 from followings.tasks import scheduler
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biblioteka.settings")
 
 application = get_wsgi_application()
 
