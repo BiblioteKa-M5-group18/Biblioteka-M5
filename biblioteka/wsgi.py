c5e0817f463b23cb0e992d18ed45e6ed1753f447
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-03-10 15:23:12 -0300

    fix: wsgi.py

--- biblioteka/wsgi.py
+++ biblioteka/wsgi.py
@@ -1,7 +1,5 @@
 import os
 
-import django
-
 from django.core.wsgi import get_wsgi_application
 
 from django_apscheduler.jobstores import register_events
@@ -9,8 +7,6 @@ from django_apscheduler.jobstores import register_events
 from followings.tasks import scheduler
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biblioteka.settings")
-
-django.setup()
 
 application = get_wsgi_application()
 
