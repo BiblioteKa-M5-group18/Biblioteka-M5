5836a69b06343adc393126e5bce8a59a005d85de
Ari <106547012+arimoncaojr@users.noreply.github.com>
2023-03-13 21:06:34 -0300

    fix: variável repetida

--- biblioteka/settings.py
+++ biblioteka/settings.py
@@ -188,13 +188,6 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
-SPECTACULAR_SETTINGS = {
-    'TITLE': 'BiblioteKA',
-    'DESCRIPTION': 'The best service to manage your library',
-    'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
-}
-
 AUTH_USER_MODEL = "users.User"
 
 SPECTACULAR_SETTINGS = {
